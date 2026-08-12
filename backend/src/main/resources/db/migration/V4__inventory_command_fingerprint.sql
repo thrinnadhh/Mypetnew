de13477edb7fcb3302f203d2840de74424e804e3
Bandapalli Trinadh <trinadhbandapalli@gmail.com>
2026-08-12 14:26:56 +0530

    make inventory replay migration portable

--- backend/src/main/resources/db/migration/V4__inventory_command_fingerprint.sql
+++ backend/src/main/resources/db/migration/V4__inventory_command_fingerprint.sql
@@ -1,9 +1,13 @@
 ALTER TABLE mypet.inventory_movement
-    ADD COLUMN operation_scope VARCHAR(40) NOT NULL DEFAULT 'legacy',
+    ADD COLUMN operation_scope VARCHAR(40) NOT NULL DEFAULT 'legacy';
+
+ALTER TABLE mypet.inventory_movement
     ADD COLUMN request_fingerprint VARCHAR(64) NOT NULL DEFAULT 'legacy';
 
 ALTER TABLE mypet.inventory_movement
-    ALTER COLUMN operation_scope DROP DEFAULT,
+    ALTER COLUMN operation_scope DROP DEFAULT;
+
+ALTER TABLE mypet.inventory_movement
     ALTER COLUMN request_fingerprint DROP DEFAULT;
 
 CREATE INDEX idx_inventory_movement_listing_occurred

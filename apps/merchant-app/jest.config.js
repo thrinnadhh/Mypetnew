50fd335970e5f28ef96cf9bc013c47f5ab974b59
Bandapalli Trinadh <trinadhbandapalli@gmail.com>
2026-08-13 19:52:03 +0530

    M1A load Merchant Jest native mocks

--- apps/merchant-app/jest.config.js
+++ apps/merchant-app/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: "jest-expo",
+  setupFiles: ["<rootDir>/jest.setup.js"],
   testMatch: ["<rootDir>/**/*.test.ts", "<rootDir>/**/*.test.tsx"],
   collectCoverageFrom: ["src/**/*.{ts,tsx}", "app/**/*.{ts,tsx}"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"]

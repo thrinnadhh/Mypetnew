a7b0c5b577d4e1e19eaa40f880d21ba906613b41
thrinnadhh <thrinnadhh.b97@gmail.com>
2026-09-01 14:54:43 +0530

    fix(whitespace): ensure single EOF newline in jest.config.js files

--- apps/merchant-app/jest.config.js
+++ apps/merchant-app/jest.config.js
@@ -22,4 +22,3 @@ module.exports = {
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|decode-uri-component)",
   ],
 };
-

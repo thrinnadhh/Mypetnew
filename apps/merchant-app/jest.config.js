3412001ed043340fda9bb51ffdf449cf46cbe597
thrinnadhh <thrinnadhh.b97@gmail.com>
2026-09-01 14:53:43 +0530

    fix(jest): configure transformIgnorePatterns for decode-uri-component

--- apps/merchant-app/jest.config.js
+++ apps/merchant-app/jest.config.js
@@ -17,5 +17,9 @@ module.exports = {
       lines: 90
     }
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"]
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|decode-uri-component)",
+  ],
 };
+
